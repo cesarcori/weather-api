8cb60ec04cef5d2f83719d6c666359a4788f8ede
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-16 20:07:16 -0400

    add: api's url with correct access

--- weather-api.py
+++ weather-api.py
@@ -1,9 +1,12 @@
-import request
+import requests
 
-APY_KEY = "508ea5b6ada26fb9a2cbb16579af96c9"
-longitude = ""
-latitude = ""
+# Change the API key for your own.
+API_KEY = "508ea5b6ada26fb9a2cbb16579af96c9"
+lat, lon = ["-16.707881", "-70.584655"]
 
-# How to write the URL
-response = request.get("")
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+
+response = requests.get(url)
+
+# print(response)
 
