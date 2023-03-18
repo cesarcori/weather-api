c3eb62bb60f08cae8a9a0fb393ab0ee665de773f
Julio Cesar Cori Ochoa <jcesarcori@gmail.com>
2023-03-18 17:09:52 -0400

    add: chooose one option

--- weather-api.py
+++ weather-api.py
@@ -3,18 +3,21 @@ import csv
 
 # Change the API key for your own.
 API_KEY = "508ea5b6ada26fb9a2cbb16579af96c9"
-lat, lon = ["-16.707881", "-70.584655"]
 
-# Read files txt
+# Read the file csv 
 with open("./coordinates.csv") as file:
     csv_reader = csv.reader(file, delimiter=",")
     locations = [locate for locate in csv_reader]
 
-    # locations.pop(0)
-    # print(locations)
-    # for n in range(len(locations)):
-        # print(n+1, ":", locations[2])
-    # location = input("content")
+# Show the options to choose
+locations.pop(0)
+print("\n*** Weather location ***\n")
+for n in range(len(locations)):
+    print(n+1, locations[n][2], sep=":")
+
+# User choose one option
+index = int(input("\nChoose one location: ")) - 1
+lat, lon, loc = float(locations[index][0]), float(locations[index][1]), locations[index][2]
 
 # Read API
 url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
@@ -25,9 +28,10 @@ if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
     temperature = round(data['main']["temp"] - 273.15, 2) # kelvin to celsius
+    print("\nLocation:", loc)
     print("Weather:", weather)
     print("Temperature:", temperature, "celsius")
 else:
-    print("An error has ocurred")
+    print("\nAn error has ocurred")
 
 
